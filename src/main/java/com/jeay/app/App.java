3b524f6b128d24395c57d69457d22fad54f0b869
Ubuntu <ubuntu@ip-172-31-91-122.ec2.internal>
2017-11-16 14:40:06 +0000

    Initial java project for Maven

---
+++ src/main/java/com/jeay/app/App.java
@@ -0,0 +1,13 @@
+package com.jeay.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! This is my first java project in maven." );
+    }
+}
