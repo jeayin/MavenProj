064939c8f2e0650a41f4b8a49d2403bdf23e8e60
Kandasamy <jayaprakash.kandasamy@cgi.com>
2017-11-29 17:56:10 +0530

    Maven paht added

--- script/build.sh
+++ script/build.sh
@@ -1,2 +1,3 @@
 echo "mvn deploy:deploy-file -DgroupId=com.jeay.app -DartifactId=$1 -Dversion=1.0.$2 -DgeneratePom=true -Dpackage=jar -Drepository=nexus -Durl=http://192.168.25.132:8081/repository/maven-releases/ -Dfile=JavaProj-1.0.$2.jar" >> args.txt
+export PATH=$PATH:/usr/lib/apache-maven-3.5.2/bin
 mvn deploy:deploy-file -DgroupId=com.jeay.app -DartifactId=$1 -Dversion=1.0.$2 -DgeneratePom=true -Dpackage=jar -Drepository=nexus -Durl=http://192.168.25.132:8081/repository/maven-releases/ -Dfile=target/JavaProj-1.0.$2.jar
