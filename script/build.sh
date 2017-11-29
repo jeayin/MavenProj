c35ddd096b0b4a8b4cfdbaae17eae5f1d2e6f572
Kandasamy <jayaprakash.kandasamy@cgi.com>
2017-11-29 17:21:38 +0530

    Just changed to trigger build

--- script/build.sh
+++ script/build.sh
@@ -1,2 +1,2 @@
-echo "mvn deploy:deploy-file -DgroupId=com.jeay.app -DartifactId=$1 -Dversion=1.0.$2 -DgeneratePom=true -Dpackage=jar -Drepository=nexus -Durl=http://192.168.25.132:8081/repository/maven-releases/ -Dfile=JavaProj-1.0.$2.jar" > args.txt
+echo "mvn deploy:deploy-file -DgroupId=com.jeay.app -DartifactId=$1 -Dversion=1.0.$2 -DgeneratePom=true -Dpackage=jar -Drepository=nexus -Durl=http://192.168.25.132:8081/repository/maven-releases/ -Dfile=JavaProj-1.0.$2.jar" >> args.txt
 mvn deploy:deploy-file -DgroupId=com.jeay.app -DartifactId=$1 -Dversion=1.0.$2 -DgeneratePom=true -Dpackage=jar -Drepository=nexus -Durl=http://192.168.25.132:8081/repository/maven-releases/ -Dfile=target/JavaProj-1.0.$2.jar
