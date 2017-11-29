28a3e9b439848b8c940475def821390eb6fb2969
Kandasamy <jayaprakash.kandasamy@cgi.com>
2017-11-29 19:29:52 +0530

    Repo auth added

--- script/build.sh
+++ script/build.sh
@@ -6,4 +6,4 @@ echo "args 2:"$2 >> script/args.txt
 echo "args 3:"$3 >> script/args.txt
 
 export PATH=$PATH:/usr/lib/apache-maven-3.5.2/bin
-mvn deploy:deploy-file -DgroupId=com.jeay.app -DartifactId=$1 -Dversion=1.0.$2 -DgeneratePom=true -Dpackage=jar -Drepository=nexus -Durl=http://192.168.25.132:8081/repository/maven-releases/ -Dfile=target/JavaProj-1.0.$2.jar
+mvn deploy:deploy-file -DgroupId=comjeayapp -DartifactId=$1 -Dversion=1.0.$2 -DgeneratePom=true -Dpackage=jar -Drepository=nexus -Durl=http://192.168.25.132:8081/repository/maven-releases/ -Dfile=target/JavaProj-1.0.$2.jar
