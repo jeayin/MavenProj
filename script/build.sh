3ae4e630b59c850e6e350c0a12359c0ddb0e250a
Kandasamy <jayaprakash.kandasamy@cgi.com>
2017-12-12 19:06:13 +0530

    Nexus Repo details updated

--- script/build.sh
+++ script/build.sh
@@ -6,4 +6,5 @@ echo "args 2:"$2 >> script/args.txt
 echo "args 3:"$3 >> script/args.txt
 
 export PATH=$PATH:/usr/lib/apache-maven-3.5.2/bin
-mvn deploy:deploy-file -DgroupId=nexusRepo -DartifactId=$1 -Dversion=1.0.$2 -DgeneratePom=false -DpomFile=pom.xml -Dpackage=jar -Drepository=nexusRepo -Durl=http://192.168.25.132:8081/repository/maven-releases/ -Dfile=target/JavaProj-1.0.$2.jar
+
+mvn -X deploy:deploy-file -DgroupId=releases -DrepositoryId=releases -DartifactId=$1 -Dversion=1.0.$2 -DgeneratePom=false -DpomFile=pom.xml -Dpackage=jar -Drepository=nexusRepo -Durl=http://192.168.25.132:8081/repository/maven-releases -Dfile=target/JavaProj-1.0.$2.jar
