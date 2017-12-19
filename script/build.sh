a9b7a300de5fe6f606c84c7275456981db7bfa07
Kandasamy <jayaprakash.kandasamy@cgi.com>
2017-12-19 16:06:42 +0530

    Disabled site

--- script/build.sh
+++ script/build.sh
@@ -8,3 +8,5 @@ echo "args 3:"$3 >> script/args.txt
 export PATH=$PATH:/usr/lib/apache-maven-3.5.2/bin
 
 mvn -X deploy:deploy-file -DgroupId=releases -DrepositoryId=releases -DartifactId=$1 -Dversion=1.0.$2 -DgeneratePom=false -DpomFile=pom.xml -Dpackage=jar -Drepository=nexusRepo -Durl=http://192.168.25.132:8081/repository/maven-releases -Dfile=target/JavaProj-1.0.$2.jar
+
+mvn -X dependency:get -Dartifactid=MAV-MAV -DgroupId=releases -Dversion=1.0.16 -DremoteRepositories=http://192.168.25.132:8081/repository/maven-releases -DartifactId=MAV-MAV -DrepositoryId=releases
